f5e21569e56bfdddd22bcabe1dbcb6881924ff44
ashok <ashokkmrakg@gmail.com>
2020-06-11 20:49:59 +0530

    remove constants

---
+++ YouTube-OneDayBuild/ConstantTemplate.swift
@@ -0,0 +1,14 @@
+//
+//  ConstantTemplate.swift
+//  YouTube-OneDayBuild
+//
+//  Created by Admin on 11/06/20.
+//  Copyright © 2020 Ashok@apponward. All rights reserved.
+//
+
+import Foundation
+//struct Constants {
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+//}
